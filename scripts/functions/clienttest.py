ab098db0fbf038046c9ff5a88aa47274c0d1692c
Rafael <reifell@github.com>
2016-09-09 11:11:43 -0300

    finish script to test the chain with hping

--- scripts/functions/clienttest.py
+++ scripts/functions/clienttest.py
@@ -31,38 +31,45 @@ c1Avg = []
 c2Min = []
 c2Max = []
 c2Avg = []
-for n in range(1,200):
-    ipId = randint(0,65500)
-    while ipId in id:
-        ipId = randint(0, 65500)
-    # p = subprocess.Popen('hping3 --udp -p 5010 -o %s --id %d  -S 10.0.0.2 -c 1' %(ecn, ipId), shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
-    # ipId1 = randint(0, 65500)
-    # p.wait()
-    ipId1 = randint(0, 65500)
-    while ipId1 in id:
-        ipId1 = randint(0, 65500)
-    # p1 = subprocess.Popen('hping3 --udp -p 5011 -o %s --id %d  -S 10.0.0.2 -c 1' %(ecn, ipId1), shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
-    # p1.wait()
-    id.append(ipId)
-    id.append(ipId1)
+total = []
+for i in range(1,5):
+    for n in range(1,120):
+        ipId = randint(0,65500)
+        while ipId in id:
+            ipId = randint(0, 65500)
 
-    output1 = subprocess.check_output('hping3 --udp -p 5010 -o %s --id %d  -S 10.0.0.2 -c 1 || true' %(ecn, ipId), shell=True,  universal_newlines=True, stderr=subprocess.STDOUT, preexec_fn=os.setsid)
+        #ipId1 = randint(0, 65500)
+        #while ipId1 in id:
+        #    ipId1 = randint(0, 65500)
 
-    output2 = subprocess.check_output('hping3 --udp -p 5011 -o %s --id %d  -S 10.0.0.2 -c 1 || true' %(ecn, ipId1), shell=True,  universal_newlines=True, stderr=subprocess.STDOUT, preexec_fn=os.setsid)
-    res = parsePing(output1)
-    if (res != None):
+        #id.append(ipId)
+        #id.append(ipId1)
 
-        c1Avg.append(res[1])
-    res = parsePing(output2)
-    if (res != None):
-        c2Avg.append(res[1])
+        output1 = subprocess.check_output('hping3 --udp -p 5010 -o %s --id %d  -S 10.0.0.2 -c 1 || true' %(ecn, ipId), shell=True,  universal_newlines=True, stderr=subprocess.STDOUT, preexec_fn=os.setsid)
 
+        #output2 = subprocess.check_output('hping3 --udp -p 5011 -o %s --id %d  -S 10.0.0.2 -c 1 || true' %(ecn, ipId1), shell=True,  universal_newlines=True, stderr=subprocess.STDOUT, preexec_fn=os.setsid)
+        time.sleep(1)
+        res = parsePing(output1)
+        if (res != None):
+            c1Min.append(res[0])
+            c1Avg.append(res[1])
+            c1Max.append(res[2])
 
-avg = float(sum(c1Avg)) / float(max(len(c1Avg), 1))
-print "c1 avg %f for %d itens" %(avg, len(c1Avg))
+        #res = parsePing(output2)
+        #if (res != None):
+        #    c2Avg.append(res[1])
 
-avg = float(sum(c2Avg)) / float(max(len(c2Avg), 1))
-print "c2 avg %f for %d itens" %(avg, len(c2Avg))
+    avg = float(sum(c1Avg)) / float(max(len(c1Avg), 1))
+    print "c1 avg %f/%f/%f for %d itens" %(min(c1Min), avg, max(c1Max), len(c1Avg))
+    total.append(avg)
+    c1Min = []
+    c1Max = []
+    c1Avg = []
+    #avg = float(sum(c2Avg)) / float(max(len(c2Avg), 1))
+    #print "c2 avg %f for %d itens" %(avg, len(c2Avg))
+
+final = float(sum(total)) / float(max(len(total), 1))
+print "final avg %f" %final
 exit(1)
 
 
