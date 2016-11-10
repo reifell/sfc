c7dd268118dc39489006b24297224e2e9003034f
Rafael <reifell@github.com>
2016-11-10 17:48:29 -0200

    add missing files

---
+++ sfc-monitor/src/main/java/org/opendaylight/monitor/packetHandler/PacketOutSender.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright (c) 2016 Rafael Eichelberger, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.monitor.packetHandler;
+
+import org.opendaylight.monitor.packetHandler.utils.TopologyHandler;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.ServiceFunctionForwarders;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.output.action._case.OutputActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.OutputPortValues;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.*;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnectorKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.TransmitPacketInputBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Created by rafael on 7/6/16.
+ */
+public class PacketOutSender {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PacketOutSender.class);
+
+    private static final int SCHEDULED_THREAD_POOL_SIZE = 1;
+    private static final int QUEUE_SIZE = 1000;
+    private static final int ASYNC_THREAD_POOL_KEEP_ALIVE_TIME_SECS = 300;
+    private static final long SHUTDOWN_TIME = 5;
+    ThreadPoolExecutor threadPoolExecutorService = null;
+
+    PacketProcessingService rpc;
+    boolean close = false;
+
+    TopologyHandler topo = new TopologyHandler();
+
+    public PacketOutSender(RpcProviderRegistry rpcProvider) {
+        rpc = rpcProvider.getRpcService(PacketProcessingService.class);
+    }
+
+    public void init () {
+        this.threadPoolExecutorService = new ThreadPoolExecutor(SCHEDULED_THREAD_POOL_SIZE, SCHEDULED_THREAD_POOL_SIZE,
+                ASYNC_THREAD_POOL_KEEP_ALIVE_TIME_SECS, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>(QUEUE_SIZE));
+        sender sender_ = new sender(10);
+        threadPoolExecutorService.execute(sender_);
+    }
+
+    public void sendCalibrationPacket() {
+        String sendSw = null;
+
+        ServiceFunctionForwarders sffs = topo.readAllSFFs();
+        if (sffs != null) {
+            for (ServiceFunctionForwarder sff : sffs.getServiceFunctionForwarder()) {
+                String sw = sff.getServiceNode().getValue();
+                // if (sw.equals("openflow:2")) {
+                sendSw = sw;
+                //  }
+
+                if (sendSw != null) {
+                    NodeBuilder nodeBuilder = new NodeBuilder();
+                    nodeBuilder.setId(new org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId(sendSw));
+                    nodeBuilder.setKey(new NodeKey(nodeBuilder.getId()));
+
+                    InstanceIdentifier<Node> NodeII = InstanceIdentifier.builder(Nodes.class)
+                            .child(Node.class, nodeBuilder.getKey()).build();
+
+                    NodeConnectorBuilder nodeConBuilder = new NodeConnectorBuilder();
+                    nodeConBuilder.setId(new NodeConnectorId("openflow:2:3"));
+                    nodeConBuilder.setKey(new NodeConnectorKey(nodeConBuilder.getId()));
+
+                    InstanceIdentifier<NodeConnector> NodeConII = InstanceIdentifier.builder(Nodes.class)
+                            .child(Node.class, nodeBuilder.getKey()).child(NodeConnector.class, nodeConBuilder.getKey()).build();
+
+
+                    // arp request arp_tpa=100.0.0.101
+                    byte[] data = new byte[]{-1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 1, 8, 6, 0, 1, 8, 0, 6, 4, 0, 1, 0,
+                            0, 0, 0, 0, 1, 10, 0, 0, 1, -1, -1, -1, -1, -1, -1, 100, 0, 0, 101};
+
+                    LOG.info("pakcet sent {} time {}", sendSw, System.currentTimeMillis());
+
+                    sendPacketToTable(NodeII, NodeConII, data);
+                }
+            }
+        } else {
+            LOG.info("no SFF detected ");
+        }
+
+    }
+
+    public void sendPacketToPort(String switchNode, String port, byte[] payload) {
+        String nodeContertor = String.format("%s:%s", switchNode, port);
+        NodeBuilder nodeBuilder = new NodeBuilder();
+        nodeBuilder.setId(new org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId(switchNode));
+        nodeBuilder.setKey(new NodeKey(nodeBuilder.getId()));
+
+        InstanceIdentifier<Node> NodeII = InstanceIdentifier.builder(Nodes.class)
+                .child(Node.class, nodeBuilder.getKey()).build();
+
+        NodeConnectorBuilder nodeConBuilder = new NodeConnectorBuilder();
+        nodeConBuilder.setId(new NodeConnectorId(nodeContertor));
+        nodeConBuilder.setKey(new NodeConnectorKey(nodeConBuilder.getId()));
+
+        InstanceIdentifier<NodeConnector> NodeConII = InstanceIdentifier.builder(Nodes.class)
+                .child(Node.class, nodeBuilder.getKey()).child(NodeConnector.class, nodeConBuilder.getKey()).build();
+
+
+        sendPacket(NodeII, NodeConII, payload);
+    }
+
+    /**
+     * This method sends the packet on given output port
+     * @param nodeIID SwithID
+     * @param ncIID Node connector (ouput port)
+     * @param data packet to transmit
+     */
+    public void sendPacketToTable(InstanceIdentifier<Node> nodeIID,
+                            InstanceIdentifier<NodeConnector> ncIID,
+                            byte[] data) {
+
+        TransmitPacketInputBuilder txBuilder = new TransmitPacketInputBuilder();
+
+        List<Action> actionList = new ArrayList<Action>();
+        ActionBuilder ab = new ActionBuilder();
+
+        OutputActionBuilder output = new OutputActionBuilder();
+        output.setMaxLength(Integer.valueOf(0xffff));
+        Uri value = new Uri(OutputPortValues.TABLE.toString());
+        output.setOutputNodeConnector(value);
+        ab.setAction(new OutputActionCaseBuilder().setOutputAction(output.build()).build());
+        ab.setOrder(0);
+        ab.setKey(new ActionKey(0));
+        actionList.add(ab.build());
+
+
+        txBuilder.setPayload(data)
+                .setNode(new NodeRef(nodeIID))
+                .setAction(actionList)
+                .setEgress(new NodeConnectorRef(ncIID));
+
+        rpc.transmitPacket(txBuilder.build());
+
+    }
+
+    /**
+     * This method sends the packet on given output port
+     * @param nodeIID SwithID
+     * @param ncIID Node connector (ouput port)
+     * @param data packet to transmit
+     */
+    public void sendPacket(InstanceIdentifier<Node> nodeIID,
+                                  InstanceIdentifier<NodeConnector> ncIID,
+                                  byte[] data) {
+
+        TransmitPacketInputBuilder txBuilder = new TransmitPacketInputBuilder();
+
+        txBuilder.setPayload(data)
+                .setNode(new NodeRef(nodeIID))
+                .setEgress(new NodeConnectorRef(ncIID));
+
+        rpc.transmitPacket(txBuilder.build());
+
+    }
+
+    public void close() throws ExecutionException, InterruptedException {
+        close = true;
+        threadPoolExecutorService.shutdown();
+        if (!threadPoolExecutorService.awaitTermination(SHUTDOWN_TIME, TimeUnit.SECONDS)) {
+            LOG.error("PacketInListener Executor did not terminate in the specified time.");
+            List<Runnable> droppedTasks = threadPoolExecutorService.shutdownNow();
+            LOG.error("PacketInListener Executor was abruptly shut down. [{}] tasks will not be executed.",
+                    droppedTasks.size());
+        }
+    }
+
+    class sender implements Runnable {
+
+        long inTime;
+
+        public sender(long inTime) {
+            this.inTime = inTime;
+        }
+
+
+        public void run() {
+            while (!close) {
+                LOG.info("send calibration packet");
+                sendCalibrationPacket();
+                try {
+                    TimeUnit.SECONDS.sleep(inTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
---
+++ sfc-monitor/src/main/java/org/opendaylight/monitor/packetHandler/utils/TopologyHandler.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2016 Rafael Eichelberger, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.monitor.packetHandler.utils;
+
+import com.google.common.base.Preconditions;
+
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.sfc.provider.api.SfcDataStoreAPI;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SfName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.RenderedServicePaths;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.RenderedServicePath;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.rsp.rev140701.rendered.service.paths.rendered.service.path.RenderedServicePathHop;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.function.base.SfDataPlaneLocator;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.ServiceFunctionForwarders;
+
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarder.base.SffDataPlaneLocator;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.service.function.forwarder.ServiceFunctionDictionary;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.ServiceFunctions;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.IpPortLocator;
+import org.opendaylight.yang.gen.v1.urn.ericsson.params.xml.ns.yang.sfc.sff.ofs.rev150408.SffDataPlaneLocator1;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPointKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by rafael on 5/19/16.
+ */
+public class TopologyHandler {
+    private InstanceIdentifier.InstanceIdentifierBuilder<Nodes> nodesInsIdBuilder = InstanceIdentifier.<Nodes>builder(Nodes.class);
+
+
+
+
+    private final InstanceIdentifier<NetworkTopology> networkII
+            = InstanceIdentifier.builder(NetworkTopology.class).build();
+
+    InstanceIdentifier<ServiceFunctionForwarders> sffIID =
+            InstanceIdentifier.create(ServiceFunctionForwarders.class);
+
+
+
+    public InstanceIdentifier<Topology> getTopologyII() throws ExecutionException, InterruptedException {
+
+        TopologyId tid = new TopologyId("flow:1");
+        TopologyKey key = new TopologyKey(Preconditions.checkNotNull(tid));
+        InstanceIdentifier<Topology> topologyII = InstanceIdentifier.create(NetworkTopology.class).child(Topology.class, key);
+        return topologyII;
+    }
+
+
+
+
+    public TerminationPoint readTerminationPoint(String bridgeName, String portName) {
+        NodeId nodeId = new NodeId(bridgeName);
+        InstanceIdentifier<TerminationPoint> tpIid =
+                InstanceIdentifier.create(NetworkTopology.class)
+                        .child(Topology.class, new TopologyKey(new TopologyId(new Uri("flow:1"))))
+                        .child(Node.class, new NodeKey(new NodeId(nodeId)))
+                        .child(TerminationPoint.class, new TerminationPointKey(new TpId(String.valueOf(portName))));
+
+        return SfcDataStoreAPI.readTransactionAPI(tpIid, LogicalDatastoreType.OPERATIONAL);
+    }
+
+
+
+    public Node readNode(String bridgeName) {
+        NodeId nodeId = new NodeId(bridgeName);
+        InstanceIdentifier<Node> tpIid =
+                InstanceIdentifier.create(NetworkTopology.class)
+                        .child(Topology.class, new TopologyKey(new TopologyId(new Uri("flow:1"))))
+                        .child(Node.class, new NodeKey(new NodeId(nodeId)));
+
+        return SfcDataStoreAPI.readTransactionAPI(tpIid, LogicalDatastoreType.OPERATIONAL);
+    }
+
+    public ServiceFunctionForwarder readSFF(String bridgeName) {
+
+//
+//        InstanceIdentifier<ServiceFunctionForwarders> sffIID =
+//                InstanceIdentifier.create(ServiceFunctionForwarders.class).child(ServiceNode.class, new ServiceNodeKey(new SnName("" )));
+
+        ServiceFunctionForwarders sffs = SfcDataStoreAPI.readTransactionAPI(sffIID, LogicalDatastoreType.CONFIGURATION);
+        for (ServiceFunctionForwarder sff : sffs.getServiceFunctionForwarder()) {
+            if (bridgeName.equals(sff.getServiceNode().getValue())) {
+                return sff;
+            }
+        }
+        return null;
+
+    }
+
+
+    public String readSfDplFromSff(ServiceFunctionForwarder sff, String port) {
+        String dplName = null;
+        for(SffDataPlaneLocator dpl : sff.getSffDataPlaneLocator()) {
+            SffDataPlaneLocator1 dplPort = dpl.getAugmentation(SffDataPlaneLocator1.class);
+            if (dplPort != null) {
+                if (dplPort.getOfsPort().getPortId().equals(port)) {
+                    dplName = dpl.getName().getValue();
+                    break;
+                }
+            }
+        }
+        String sfDpl = null;
+        if (new String("egress").equals(dplName)) {
+            return dplName;
+        }
+        if (dplName != null) {
+            for ( ServiceFunctionDictionary sf : sff.getServiceFunctionDictionary()) {
+                if (sf.getSffSfDataPlaneLocator().getSffDplName().getValue().equals(dplName)) {
+                    sfDpl = sf.getSffSfDataPlaneLocator().getSfDplName().getValue();
+                }
+            }
+        }
+        return sfDpl;
+    }
+
+    public ServiceFunctionForwarders readAllSFFs() {
+            return SfcDataStoreAPI.readTransactionAPI(sffIID, LogicalDatastoreType.CONFIGURATION);
+    }
+
+    public ServiceFunction readSfName(String dpl) {
+        if (dpl != null) {
+            InstanceIdentifier<ServiceFunctions> sfIID =
+                    InstanceIdentifier.create(ServiceFunctions.class);
+            ServiceFunctions sfs = SfcDataStoreAPI.readTransactionAPI(sfIID, LogicalDatastoreType.CONFIGURATION);
+
+            for (ServiceFunction sf : sfs.getServiceFunction()) {
+                for (SfDataPlaneLocator sfDpl : sf.getSfDataPlaneLocator()) {
+                    if (sfDpl.getName().getValue().equals(dpl)) {
+                        return sf;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public ServiceFunction readSfNameByIp(String Ip) {
+        if (Ip != null) {
+            InstanceIdentifier<ServiceFunctions> sfIID =
+                    InstanceIdentifier.create(ServiceFunctions.class);
+            ServiceFunctions sfs = SfcDataStoreAPI.readTransactionAPI(sfIID, LogicalDatastoreType.CONFIGURATION);
+
+            for (ServiceFunction sf : sfs.getServiceFunction()) {
+                for (SfDataPlaneLocator sfDpl : sf.getSfDataPlaneLocator()) {
+                    IpPortLocator dstSfLocator = (IpPortLocator) sfDpl.getLocatorType();
+                    String sfIp = new String(dstSfLocator.getIp().getValue());
+                    if (sfIp.equals(sfIp)) {
+                        return sf;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public ServiceFunction readSfNameByRsp(int nsp, int nsi) {
+        InstanceIdentifier<RenderedServicePaths> rspIID =
+                InstanceIdentifier.create(RenderedServicePaths.class);
+        RenderedServicePaths rsps = SfcDataStoreAPI.readTransactionAPI(rspIID, LogicalDatastoreType.OPERATIONAL);
+        SfName sfName = null;
+        for (RenderedServicePath rsp : rsps.getRenderedServicePath()) {
+            if (nsp == rsp.getPathId()) {
+
+                for (RenderedServicePathHop hop : rsp.getRenderedServicePathHop()) {
+                    if ((255 - nsi) == hop.getHopNumber()) {
+                        sfName = hop.getServiceFunctionName();
+                        break;
+                    }
+                }
+            }
+        }
+        if (sfName != null) {
+            InstanceIdentifier<ServiceFunctions> sfIID =
+                    InstanceIdentifier.create(ServiceFunctions.class);
+            ServiceFunctions sfs = SfcDataStoreAPI.readTransactionAPI(sfIID, LogicalDatastoreType.CONFIGURATION);
+            for (ServiceFunction sf : sfs.getServiceFunction()) {
+                if (sfName.equals(sf.getName())) {
+                    return sf;
+                }
+            }
+
+        }
+        return null;
+    }
+
+    }
